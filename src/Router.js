18d2c5506fe92d5c5b0158be5beef26cee10898d
jadsonlucena <jadsonlucena@gmail.com>
2022-02-19 10:36:15 -0300

    feat (private method): check if string is url

--- src/Router.js
+++ src/Router.js
@@ -2,4 +2,18 @@ class Router {
 
     constructor() {}
 
+    #isURL(href) {
+
+        try {
+
+            return new URL(href);
+
+        } catch (err) {
+
+            return false;
+
+        }
+
+    }
+
 }
