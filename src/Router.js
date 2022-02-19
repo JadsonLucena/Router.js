b1db81288207470b5eba06533f3e64939a48b8c1
jadsonlucena <jadsonlucena@gmail.com>
2022-02-19 10:36:49 -0300

    feat (private method): load the html content

--- src/Router.js
+++ src/Router.js
@@ -16,4 +16,61 @@ class Router {
 
     }
 
+    #loadFragments(fragments) {
+
+        return Promise.allSettled(fragments.map(fragment => new Promise((resolve, reject) => {
+
+            try {
+
+                let url = this.#isURL(fragment);
+                if (url) {
+
+                    fetch(fragment, {
+                        headers: {
+                            'Accept': 'text/html;charset=UTF-8'
+                        }
+                    }).then(res => {
+
+                        if (res.ok) {
+
+                            res.text().then(fragment => {
+
+                                let children = Array.from(new DOMParser().parseFromString(fragment, 'text/html').body.children);
+
+                                children.forEach(fragment => fragment.classList.add('router-fragment'));
+
+
+                                resolve(children);
+
+                            }).catch(reject);
+
+                        } else {
+
+                            reject(res);
+
+                        }
+
+                    }).catch(reject);
+
+                } else  {
+
+                    let children = Array.from(new DOMParser().parseFromString(fragment, 'text/html').body.children);
+
+                    children.forEach(fragment => fragment.classList.add('router-fragment'));
+
+
+                    resolve(children);
+
+                }
+
+            } catch (err) {
+
+                reject(err);
+
+            }
+
+        })));
+
+    }
+
 }
